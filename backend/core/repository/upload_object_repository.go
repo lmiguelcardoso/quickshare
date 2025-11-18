0bfc4d023b3253342137abae19f6883fc9540db4
Miguel <mi_lmc@hotmail.com>
2025-11-17 21:06:21 -0300

    feat (backend): add PostgreSQLRepository interface for managing upload objects

---
+++ backend/core/repository/upload_object_repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"quickshare/core/model"
+)
+
+type PostgreSQLRepository interface {
+	CreateUploadObject(uploadObject *model.UploadObject) (*model.UploadObject, error)
+	GetUploadObject(id string) (*model.UploadObject, error)
+	UpdateUploadObjectStatus(id string, status string) error
+	DeleteUploadObject(id string) error
+}
