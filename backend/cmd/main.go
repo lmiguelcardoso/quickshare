3b5c361345a1eae52cf6d55b6a38ffb5dbb15e7b
Miguel <mi_lmc@hotmail.com>
2025-11-18 07:57:54 -0300

    feat (backend): update docker-compose and main.go for PostgreSQL integration and configuration management

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	httphandler "quickshare/adapter/http"
+	"quickshare/internal/config"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
-const port = ":8080"
 
 func main() {
 	handler := httphandler.NewHandler()
 	router := mux.NewRouter()
-	
+	cfg := config.NewConfig()
 	handler.RegisterRoutes(router)
-	log.Println("Server is running on port", port)
+	log.Println("Server is running on port", cfg.ServerConfig.Port)
 
-	err := http.ListenAndServe(port, router); if err != nil {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", cfg.DBConfig.DBHost, cfg.DBConfig.DBPort, cfg.DBConfig.DBUser, cfg.DBConfig.DBPassword, cfg.DBConfig.DBName)
+	
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	log.Println("Connected to database")
+
+	err = http.ListenAndServe(cfg.ServerConfig.Port, router)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
