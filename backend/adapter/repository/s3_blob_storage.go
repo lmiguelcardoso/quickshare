716725d0a98465b301f1b1569562e3123dfe84d4
Miguel <mi_lmc@hotmail.com>
2025-11-20 23:39:00 -0300

    feat (backend): add methods to S3BlobStorage for public URL generation and object existence check

--- backend/adapter/repository/s3_blob_storage.go
+++ backend/adapter/repository/s3_blob_storage.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
@@ -37,7 +38,7 @@ func NewS3BlobStorage(s3Client *s3.S3, bucket string, baseURL string, uploadDir 
 }
 
 func (s *S3BlobStorage) GeneratePresignedUploadURL(objectKey string, expiresIn time.Duration) (string, error){
-	req, _ := s.s3Client.GetObjectRequest(&s3.GetObjectInput{
+	req, _ := s.s3Client.PutObjectRequest(&s3.PutObjectInput{
 		Bucket: aws.String(s.bucket),
 		Key: aws.String(objectKey),
 	})
@@ -52,4 +53,29 @@ func (s *S3BlobStorage) GeneratePresignedUploadURL(objectKey string, expiresIn t
 	log.Printf("presigned URL generated successfully: %s", urlStr)
 
 	return urlStr, nil
+}
+
+func (s *S3BlobStorage) GetPublicURL(objectKey string) string {
+	return fmt.Sprintf("%s/%s", s.baseURL, objectKey)
+}
+
+func (s *S3BlobStorage) ObjectExists(objectKey string) (bool, error) {
+	_, err := s.s3Client.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(objectKey),
+	})
+
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchKey:
+				return false, nil
+			default:
+				return false, fmt.Errorf("failed to check if object exists: %w", err)
+			}
+		}
+		return false, fmt.Errorf("failed to check if object exists: %w", err)
+	}
+
+	return true, nil
 }
